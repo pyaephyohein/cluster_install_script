7ba28be7be6b9e68392183d63de74ba76adde08d
Pyae Phyo Hein <pyaephyohein.info.3326@gmail.com>
2020-10-24 18:09:12 +0630

    kube version 1.17.1-00

--- clusterinstall.sh
+++ clusterinstall.sh
@@ -4,7 +4,7 @@ apt install vim
 echo "Enter HostName"
 read hostname
 hostnamectl set-hostname $hostname
-echo "1 deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/sources.list.d/kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 apt update -y
 apt install -y kubeadm=1.17.1-00 kubelet=1.17.1-00 kubectl=1.17.1-00
