b0ef6b04e10b935a46d665c1d4836d0e3db0094b
Pyae Phyo Hein <pyaephyohein.info.3326@gmail.com>
2020-10-26 23:01:39 +0630

    add docker install

--- clusterinstall.sh
+++ clusterinstall.sh
@@ -1,6 +1,7 @@
 apt update -y
 apt upgrade -y
 apt install vim
+apt install docker.io -y
 echo "Enter HostName"
 read hostname
 hostnamectl set-hostname $hostname
@@ -9,3 +10,5 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 apt update -y
 apt install -y kubeadm=1.17.1-00 kubelet=1.17.1-00 kubectl=1.17.1-00
 apt-mark hold kubelet kubeadm kubectl
+systemctl enable docker.service
+systemctl restart docker.servce
